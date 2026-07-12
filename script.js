03b4200b0c885ffbbf2587e2b749003d05434cfc
jaydhas238-beep <jaydhas238@gmail.com>
2026-07-12 22:59:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -227,3 +227,22 @@ function trackComplaint(){
     `;
 
 }
+
+function login(){
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if(username === "admin" && password === "admin123"){
+
+        localStorage.setItem("adminLoggedIn","true");
+
+        window.location.href = "admin.html";
+
+    }else{
+
+        alert("Invalid Username or Password");
+
+    }
+
+}
