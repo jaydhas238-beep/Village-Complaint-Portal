445f5faba046cacc6cf80c9e17e21281636ca939
jaydhas238-beep <jaydhas238@gmail.com>
2026-07-12 22:24:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+const form = document.getElementById("complaintForm");
 
+if (form) {
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const name = document.getElementById("name").value;
+        const mobile = document.getElementById("mobile").value;
+        const category = document.getElementById("category").value;
+        const description = document.getElementById("description").value;
+        const photo = document.getElementById("photo").files[0];
+
+        if (!photo) {
+            alert("Please upload a photo.");
+            return;
+        }
+
+        const reader = new FileReader();
+
+        reader.onload = function () {
+
+            let complaints = JSON.parse(localStorage.getItem("complaints")) || [];
+
+            const complaint = {
+                id: "VC" + String(complaints.length + 1).padStart(3, "0"),
+                name,
+                mobile,
+                category,
+                description,
+                photo: reader.result,
+                status: "Pending"
+            };
+
+            complaints.push(complaint);
+
+            localStorage.setItem("complaints", JSON.stringify(complaints));
+
+            alert("Complaint Submitted Successfully!\nComplaint ID: " + complaint.id);
+
+            form.reset();
+        };
+
+        reader.readAsDataURL(photo);
+
+    });
+}
